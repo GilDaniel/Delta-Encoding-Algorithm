484acb09e72d3110f7586d24b0a77529d9bc84bc
Gil Daniel <gildanielsfernandes@gmail.com>
2025-11-09 22:38:37 -0300

    Translate to english and add ascii art

--- src/Compression/Compression.h
+++ src/Compression/Compression.h
@@ -5,6 +5,7 @@
     class Compression{
 
         public:
+
             // Compress returns a byte buffer (char*) containing the compressed representation
             // Decompress receives that byte buffer and the original data size and returns an int array
             virtual char* Compress(int* data,int dataSize) = 0;
